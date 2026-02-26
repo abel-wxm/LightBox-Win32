b4702b5ba1605d409dd966e23a2731d6603fc27e
abel-wxm <abel.wxm@gmail.com>
2026-02-26 11:50:23 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,58 +1,95 @@
 #include <windows.h>
-#include <shellapi.h>
+#include <shobjidl.h>
+#include <shlobj.h>
 #include <string>
-#include <vector>
+#include <shellapi.h>
 
-#pragma warning(disable: 4996)
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "uuid.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "user32.lib")
 
-const char* TARGET_PATH = "C:\\Users\\xm\\Desktop\\DesktopBox\\";
+const std::wstring DESKTOP_PATH = L"C:\\Users\\xm\\Desktop\\";
+const std::wstring BOX_PATH = L"C:\\Users\\xm\\Desktop\\DesktopBox_1\\";
 
-struct BoxItem {
-    std::string name;
-    HICON hIcon;
-};
-std::vector<BoxItem> g_items;
-int g_width = 320;
+IExplorerBrowser* g_peb = nullptr;
 int g_alpha = 200;
 
-void RefreshBox(HWND hwnd) {
-    for (size_t i = 0; i < g_items.size(); i++) DestroyIcon(g_items[i].hIcon);
-    g_items.clear();
+void MoveFilesBackAndClean() {
+    WCHAR src[MAX_PATH + 2] = {0};
+    wcscpy_s(src, BOX_PATH.c_str());
+    src[wcslen(src)] = L'*'; 
 
-    WIN32_FIND_DATAA fd;
-    std::string search = std::string(TARGET_PATH) + "*";
-    HANDLE hFind = FindFirstFileA(search.c_str(), &fd);
-    if (hFind != INVALID_HANDLE_VALUE) {
-        do {
-            if (strcmp(fd.cFileName, ".") != 0 && strcmp(fd.cFileName, "..") != 0) {
-                BoxItem item;
-                item.name = fd.cFileName;
-                std::string fullPath = std::string(TARGET_PATH) + fd.cFileName;
-                
-                SHFILEINFOA sfi = {0};
-                SHGetFileInfoA(fullPath.c_str(), 0, &sfi, sizeof(sfi), SHGFI_ICON | SHGFI_LARGEICON);
-                item.hIcon = sfi.hIcon;
-                g_items.push_back(item);
-            }
-        } while (FindNextFileA(hFind, &fd));
-        FindClose(hFind);
-    }
-    InvalidateRect(hwnd, NULL, TRUE);
+    WCHAR dst[MAX_PATH + 2] = {0};
+    wcscpy_s(dst, DESKTOP_PATH.c_str());
+
+    SHFILEOPSTRUCTW sfo = {0};
+    sfo.wFunc = FO_MOVE;
+    sfo.pFrom = src;
+    sfo.pTo = dst;
+    sfo.fFlags = FOF_NOCONFIRMATION | FOF_SILENT | FOF_NOERRORUI;
+    SHFileOperationW(&sfo);
+
+    RemoveDirectoryW(BOX_PATH.c_str());
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
-        case WM_CREATE:
-            CreateDirectoryA(TARGET_PATH, NULL);
-            DragAcceptFiles(hwnd, TRUE);
-            RefreshBox(hwnd);
+        case WM_CREATE: {
+            CreateDirectoryW(BOX_PATH.c_str(), NULL);
+
+            if (SUCCEEDED(CoCreateInstance(CLSID_ExplorerBrowser, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&g_peb)))) {
+                RECT rc;
+                GetClientRect(hwnd, &rc);
+                
+                FOLDERSETTINGS fs = {0};
+                fs.ViewMode = FVM_ICON;
+                fs.fFlags = FWF_AUTOARRANGE | FWF_NOWEBVIEW | FWF_HIDEFILENAMES | FWF_TRANSPARENT;
+                
+                g_peb->Initialize(hwnd, &rc, &fs);
+                g_peb->SetOptions(EBO_NAVIGATEONCE);
+                
+                PIDLIST_ABSOLUTE pidl;
+                if (SUCCEEDED(SHParseDisplayName(BOX_PATH.c_str(), NULL, &pidl, 0, NULL))) {
+                    g_peb->BrowseToIDList(pidl, SBSP_ABSOLUTE);
+                    ILFree(pidl);
+                }
+            }
             break;
-            
-        case WM_SIZE:
-            g_width = LOWORD(lParam);
-            InvalidateRect(hwnd, NULL, TRUE);
+        }
+        case WM_SIZE: {
+            if (g_peb) {
+                RECT rc;
+                GetClientRect(hwnd, &rc);
+                g_peb->SetRect(NULL, rc);
+            }
             break;
-            
+        }
+        case WM_NCHITTEST: {
+            LRESULT hit = DefWindowProc(hwnd, uMsg, wParam, lParam);
+            if (hit == HTCLIENT) {
+                POINT pt;
+                pt.x = GET_X_LPARAM(lParam);
+                pt.y = GET_Y_LPARAM(lParam);
+                ScreenToClient(hwnd, &pt);
+                
+                RECT rc;
+                GetClientRect(hwnd, &rc);
+                int border = 10;
+                
+                if (pt.x < border && pt.y < border) return HTTOPLEFT;
+                if (pt.x > rc.right - border && pt.y < border) return HTTOPRIGHT;
+                if (pt.x < border && pt.y > rc.bottom - border) return HTBOTTOMLEFT;
+                if (pt.x > rc.right - border && pt.y > rc.bottom - border) return HTBOTTOMRIGHT;
+                if (pt.x < border) return HTLEFT;
+                if (pt.x > rc.right - border) return HTRIGHT;
+                if (pt.y < border) return HTTOP;
+                if (pt.y > rc.bottom - border) return HTBOTTOM;
+                
+                return HTCAPTION; 
+            }
+            return hit;
+        }
         case WM_MOUSEWHEEL: {
             int zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
             if (zDelta > 0 && g_alpha < 255) g_alpha += 15;
@@ -62,91 +99,15 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             SetLayeredWindowAttributes(hwnd, 0, g_alpha, LWA_ALPHA);
             break;
         }
-
-        case WM_DROPFILES: {
-            HDROP hDrop = (HDROP)wParam;
-            UINT count = DragQueryFileA(hDrop, 0xFFFFFFFF, NULL, 0);
-            for (UINT i = 0; i < count; i++) {
-                char src[MAX_PATH] = {0};
-                DragQueryFileA(hDrop, i, src, MAX_PATH);
-                
-                char doubleNullSrc[MAX_PATH + 2] = {0};
-                strcpy(doubleNullSrc, src);
-                
-                char doubleNullDst[MAX_PATH + 2] = {0};
-                strcpy(doubleNullDst, TARGET_PATH);
-                
-                SHFILEOPSTRUCTA sfo = {0};
-                sfo.hwnd = hwnd;
-                sfo.wFunc = FO_MOVE;
-                sfo.pFrom = doubleNullSrc;
-                sfo.pTo = doubleNullDst;
-                sfo.fFlags = FOF_NOCONFIRMATION | FOF_SILENT | FOF_NOERRORUI;
-                SHFileOperationA(&sfo);
+        case WM_DESTROY: {
+            if (g_peb) {
+                g_peb->Destroy();
+                g_peb->Release();
             }
-            DragFinish(hDrop);
-            RefreshBox(hwnd);
-            break;
-        }
-
-        case WM_LBUTTONDOWN: {
-            int mx = LOWORD(lParam);
-            int my = HIWORD(lParam);
-            int cols = max(1, g_width / 70);
-            int col = mx / 70;
-            int row = my / 90;
-            int idx = row * cols + col;
-            if (idx < 0 || idx >= (int)g_items.size()) {
-                ReleaseCapture();
-                SendMessage(hwnd, WM_SYSCOMMAND, 0xF012, 0);
-            }
-            break;
-        }
-
-        case WM_LBUTTONDBLCLK: {
-            int mx = LOWORD(lParam);
-            int my = HIWORD(lParam);
-            int cols = max(1, g_width / 70);
-            int col = mx / 70;
-            int row = my / 90;
-            int idx = row * cols + col;
-            if (idx >= 0 && idx < (int)g_items.size()) {
-                std::string path = std::string(TARGET_PATH) + g_items[idx].name;
-                ShellExecuteA(NULL, "open", path.c_str(), NULL, NULL, SW_SHOWNORMAL);
-            }
-            break;
-        }
-
-        case WM_PAINT: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-            
-            HBRUSH hBrush = CreateSolidBrush(RGB(25, 25, 25));
-            FillRect(hdc, &ps.rcPaint, hBrush);
-            DeleteObject(hBrush);
-
-            SetBkMode(hdc, TRANSPARENT);
-            SetTextColor(hdc, RGB(255, 255, 255));
-
-            int cols = max(1, g_width / 70);
-            for (size_t i = 0; i < g_items.size(); i++) {
-                int col = i % cols;
-                int row = i / cols;
-                int x = col * 70 + 15;
-                int y = row * 90 + 15;
-
-                DrawIcon(hdc, x, y, g_items[i].hIcon);
-                
-                RECT rc = { x - 15, y + 35, x + 47, y + 75 };
-                DrawTextA(hdc, g_items[i].name.c_str(), -1, &rc, DT_CENTER | DT_WORDBREAK | DT_NOPREFIX | DT_END_ELLIPSIS);
-            }
-            
-            EndPaint(hwnd, &ps);
-            break;
-        }
-        case WM_DESTROY:
+            MoveFilesBackAndClean();
             PostQuitMessage(0);
             break;
+        }
         default:
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
@@ -154,19 +115,20 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 }
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow) {
-    WNDCLASS wc = {0};
-    wc.style = CS_DBLCLKS; 
+    CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+
+    WNDCLASSW wc = {0};
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInst;
-    wc.lpszClassName = "LightBoxClass";
+    wc.lpszClassName = L"DesktopBoxClass";
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    RegisterClass(&wc);
+    RegisterClassW(&wc);
 
-    HWND hwnd = CreateWindowEx(
+    HWND hwnd = CreateWindowExW(
         WS_EX_TOPMOST | WS_EX_LAYERED, 
-        "LightBoxClass", "LightBox", 
+        L"DesktopBoxClass", L"DesktopBox", 
         WS_POPUP | WS_VISIBLE | WS_THICKFRAME,
-        200, 200, 320, 450, NULL, NULL, hInst, NULL);
+        200, 200, 400, 300, NULL, NULL, hInst, NULL);
         
     SetLayeredWindowAttributes(hwnd, 0, g_alpha, LWA_ALPHA);
     
@@ -175,5 +137,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+
+    CoUninitialize();
     return 0;
 }
